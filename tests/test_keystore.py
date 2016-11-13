fa6b4e4e9ccc702acdcee254520dadc5e8fc8df4
Zander Erasmus <erazander@gmail.com>
2016-11-12 18:31:11 -0800

    Added more tests to verify keystore behaviour.

--- tests/test_keystore.py
+++ tests/test_keystore.py
@@ -85,13 +85,7 @@ def test_onelevelreplace():
     tb_ = db_.table()
     ks_tb_ = db_._storage.read()['keystore']
     
-    print(tb_.all())
-    print(ks_tb_)
-    
     tb_.insert({'myid1':1})
-    
-    print(tb_.all())
-    print(ks_tb_)
 
     assert len(tb_) == 1
     assert len(ks_tb_) == 1
@@ -104,4 +98,129 @@ def test_onelevelreplace():
     assert len(ks_tb_) == 2
     assert 'myid2' in ks_tb_.keys()
     assert ks_tb_['myid2'] == 'b'
+
+def test_twolevelreplace():
+    db_ = TinyDB(storage=KeystoreMiddleware(MemoryStorage, ['a', 'b', 'c', 'd']))
+    tb_ = db_.table()
+    ks_tb_ = db_._storage.read()['keystore']
+    
+    tb_.insert({'myid1':1, 'myid2':{'myid3':3}, 'myid4':4})
+    
+    assert len(tb_) == 1
+    assert len(ks_tb_) == 4
+    assert 'myid1' in ks_tb_.keys()
+    assert 'myid2' in ks_tb_.keys()
+    assert 'myid3' in ks_tb_.keys()
+    assert 'myid4' in ks_tb_.keys()
+    
+    assert ks_tb_['myid1'] in ['a', 'b', 'c', 'd']
+    assert ks_tb_['myid2'] in ['a', 'b', 'c', 'd']
+    assert ks_tb_['myid3'] in ['a', 'b', 'c', 'd']
+    assert ks_tb_['myid4'] in ['a', 'b', 'c', 'd']
+    
+    old_ks_tb_ = ks_tb_.copy()
+    tb_.insert({'myid4':{'myid2':2}})
+    
+    assert len(tb_) == 2
+    assert len(ks_tb_) == 4
+    assert ks_tb_['myid1'] == old_ks_tb_['myid1']
+    assert ks_tb_['myid2'] == old_ks_tb_['myid2']
+    assert ks_tb_['myid3'] == old_ks_tb_['myid3']
+    assert ks_tb_['myid4'] == old_ks_tb_['myid4']
+    
+def test_smallkeylist():
+    db_ = TinyDB(storage=KeystoreMiddleware(MemoryStorage, ['a', 'b', 'c', 'd']))
+    tb_ = db_.table()
+    ks_tb_ = db_._storage.read()['keystore']
+    
+    tb_.insert({'myid1':1, 'myid2':{'myid3':3}, 'myid4':4})
+    
+    assert len(tb_) == 1
+    assert len(ks_tb_) == 4
+    assert 'myid1' in ks_tb_.keys()
+    assert 'myid2' in ks_tb_.keys()
+    assert 'myid3' in ks_tb_.keys()
+    assert 'myid4' in ks_tb_.keys()
+    assert 'myid5' not in ks_tb_.keys()
+    assert 'myid6' not in ks_tb_.keys()
+    assert ks_tb_['myid1'] in ['a', 'b', 'c', 'd']
+    assert ks_tb_['myid2'] in ['a', 'b', 'c', 'd']
+    assert ks_tb_['myid3'] in ['a', 'b', 'c', 'd']
+    assert ks_tb_['myid4'] in ['a', 'b', 'c', 'd']
+    
+    old_ks_tb_ = ks_tb_.copy()
+    tb_.insert({'myid5':{'myid6':2}})
+    
+    assert len(tb_) == 2
+    assert len(ks_tb_) == 4
+    assert 'myid5' not in ks_tb_.keys()
+    assert 'myid6' not in ks_tb_.keys()
+    assert ks_tb_['myid1'] == old_ks_tb_['myid1']
+    assert ks_tb_['myid2'] == old_ks_tb_['myid2']
+    assert ks_tb_['myid3'] == old_ks_tb_['myid3']
+    assert ks_tb_['myid4'] == old_ks_tb_['myid4']
+
+def test_mutipletable_add():
+    db_ = TinyDB(storage=KeystoreMiddleware(MemoryStorage, ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']))
+    tb_ = [db_.table('tb1'), db_.table('tb2'), db_.table('tb3'), db_.table('tb4')]
+    ks_tb_ = db_._storage.read()['keystore']
+    
+    assert set(db_.tables()) == set(['_default', 'keystore', 'tb1', 'tb2', 'tb3', 'tb4'])
+    assert len(ks_tb_) == 0
+    assert len(tb_[0]) == 0
+    assert len(tb_[1]) == 0
+    assert len(tb_[2]) == 0
+    assert len(tb_[3]) == 0
+    
+    tb_[0].insert({'myid1':1})
+    
+    assert len(ks_tb_) == 1
+    assert len(tb_[0]) == 1
+    assert len(tb_[1]) == 0
+    assert len(tb_[2]) == 0
+    assert len(tb_[3]) == 0
+    assert 'myid1' in ks_tb_.keys()
+    assert ks_tb_['myid1'] == 'a'
+    old_ks_tb_ = ks_tb_.copy()
+    
+    tb_[1].insert({'myid1':{'myid2':'someothervalue'}})
+    
+    assert len(ks_tb_) == 2
+    assert len(tb_[0]) == 1
+    assert len(tb_[1]) == 1
+    assert len(tb_[2]) == 0
+    assert len(tb_[3]) == 0
+    assert ks_tb_['myid1'] == old_ks_tb_['myid1']
+    assert 'myid2' in ks_tb_.keys()
+    assert ks_tb_['myid2'] == 'b'
+    old_ks_tb_ = ks_tb_.copy()
+    
+    tb_[2].insert({'myid3':{'myid4':'c'}})
+    
+    assert len(ks_tb_) == 4
+    assert len(tb_[0]) == 1
+    assert len(tb_[1]) == 1
+    assert len(tb_[2]) == 1
+    assert len(tb_[3]) == 0
+    assert ks_tb_['myid1'] == old_ks_tb_['myid1']
+    assert ks_tb_['myid2'] == old_ks_tb_['myid2']
+    assert 'myid3' in ks_tb_.keys()
+    assert 'myid4' in ks_tb_.keys()
+    assert ks_tb_['myid3'] in ['c', 'd']
+    assert ks_tb_['myid4'] in ['c', 'd']
+    old_ks_tb_ = ks_tb_.copy()
+    
+    tb_[3].insert({'myid2':{'myid5':5}})
+    
+    assert len(ks_tb_) == 5
+    assert len(tb_[0]) == 1
+    assert len(tb_[1]) == 1
+    assert len(tb_[2]) == 1
+    assert len(tb_[3]) == 1
+    assert ks_tb_['myid1'] == old_ks_tb_['myid1']
+    assert ks_tb_['myid2'] == old_ks_tb_['myid2']
+    assert ks_tb_['myid3'] == old_ks_tb_['myid3']
+    assert ks_tb_['myid4'] == old_ks_tb_['myid4']
+    assert 'myid5' in ks_tb_.keys()
+    assert ks_tb_['myid5'] == 'e'
 
